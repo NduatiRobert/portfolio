dfc029e76eb94a7e6d7d4619d3b9b9b2aae3cca2
NduatiRobert <robertnjuguna01@gmail.com>
2024-10-08 22:19:53 +0300

    added my profile

--- myportfolio/src/Hero.jsx
+++ myportfolio/src/Hero.jsx
@@ -3,8 +3,18 @@ import profile from "./assets/ROBERT.JPG"
 
 const Hero = () => {
   return (
-    <div>
+    <div className='flex items-center gap-16 flex-col'>
         <img src={profile} className='rounded-full' />
+        <h3 className='font-bold text-4xl p-5'>I am Nduati Robert Njuguna, web developer in Kenya.</h3>
+        <p>I am a frontend developer from California, 
+            USA with 10 years of experience in multiple 
+            companies like Microsoft, Tesla and Apple.
+        </p>
+
+        <div className='gap-10 flex-row justify-between inline-flex p-10'>
+            <button className='m-4 p-4 bg-red-600 rounded-xl text-white font-bold'>My Resume</button>
+            <button className='m-4 p-4 bg-red-600 rounded-xl text-white font-bold'>Contact Me</button>
+        </div>
       
     </div>
   )
--- myportfolio/src/Navbar.jsx
+++ myportfolio/src/Navbar.jsx
@@ -4,10 +4,10 @@ import logo from "./assets/developer.png"
 const Navbar = () => {
   return (
     <div className= "flex justify-between items-center m-5">
-        <img  className = 'inline m-4 rounded-full'  src={logo} />
+        <img  className = 'inline mr-10 m-4 rounded-full '  src={logo} />
     <ul className="flex items-center list-none gap-10 text-xl  font-bold">
         <li className='hover:text-green-50 hover:underline'>Home</li>
-        <li  className='hover:text-green-50 hover:underline'>About Me</li>
+        <li  className='hover:text-green-50 hover:underline'>About</li>
         <li  className='hover:text-green-50 hover:underline'>Skills</li>
         <li  className='hover:text-green-50 hover:underline'>Projects</li>
     </ul>
